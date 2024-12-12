6006465e6d6005cae4831eb3e4f3bab8ea987c45
Oleh Kyrylenko <oleh.kyrylenko@esc.edu>
2024-12-12 14:20:09 -0500

    lite fixes

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useMemo} from "react";
-import PropTypes from 'prop-types';
+import React, { useState, useEffect, useMemo } from "react";
+import PropTypes from "prop-types";
 import DatePickerMain from "react-datepicker";
 import cn from "classnames";
 import DatePickerNavigation from "./components/DatePickerNavigation/DatePickerNavigation.jsx";
@@ -7,82 +7,81 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./DatePicker.scss";
 
 const DatePickerView = (props) => {
-	const {events, renderDay, renderMonth, handlerIsModal, className, category, onSelect} = props;
-	
-	const [startDate, setStartDate] = useState(new Date());
-	const [showMonthView, setShowMonthView] = useState(false);
-	
-	const handlerStartDate = (date) => setStartDate(date);
-	const handlerSetDateInput = (day) => setStartDate(day);
-	
-	const renderDayContents = (day, date) => {
-		const current = new Date(date);
-		const filteredTooltipText = events?.filter(({date}) => {
-			const currentDate = `${current.getFullYear()}${current.getMonth()}${current.getDate()}`;
-			
-			return date === currentDate;
-		});
-		
-		return (
-			<>
-				<div className={"day"}>{day}</div>
-				{filteredTooltipText.map((item, index) => (
-					<div key={index}>
-						{item?.title && <span className="label"/>}
-						
-						<span
-							onClick={(e) => {
-								handlerIsModal();
-								onSelect(date, e);
-							}}
-							className={"title"}
-						>
+  const { events, renderDay, renderMonth, handlerIsModal, className, category, onSelect } = props;
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [showMonthView, setShowMonthView] = useState(false);
+
+  const handlerStartDate = (date) => setStartDate(date);
+  const handlerSetDateInput = (day) => setStartDate(day);
+
+  const renderDayContents = (day, date) => {
+    const current = new Date(date);
+    const filteredTooltipText = events?.filter(({ date }) => {
+      const currentDate = `${current.getFullYear()}${current.getMonth()}${current.getDate()}`;
+
+      return date === currentDate;
+    });
+
+    return (
+      <>
+        <div className={"day"}>{day}</div>
+        {filteredTooltipText.map((item, index) => (
+          <div key={index}>
+            {item?.title && <span className="label" />}
+
+            <span
+              onClick={(e) => {
+                handlerIsModal();
+                onSelect(date, e);
+              }}
+              className={"title"}
+            >
               {item?.time} - {item?.title}
             </span>
-					</div>
-				))}
-			</>
-		);
-	};
-	
-	const renderMonthContent = (month, shortMonth, longMonth, day) => {
-		
-		const handlerSelectMonth = () => {
-			const fullYear = new Date(day).getFullYear();
-			setShowMonthView(false)
-			setCurrentMonth(new Date(`${fullYear}-01-${month + 1 < 10 ? `0${month + 1}` : month + 1}`))
-			handlerStartDate(new Date(`${fullYear}-01-${month + 1 < 10 ? `0${month + 1}` : month + 1}`))
-		}
-		
-		return <span onClick={handlerSelectMonth}>{shortMonth}</span>;
-	};
-	
-	return (
-		<>
-			<DatePickerNavigation
-				category={category}
-				onStartDate={handlerStartDate}
-				onShowMonthView={setShowMonthView}
-				currentDay={startDate}
-				onDateInput={handlerSetDateInput}
-				events={events}
-			/>
-			
-			<div className="wrapper-date-picker">
-				<DatePickerMain
-					{...props}
-					calendarClassName={cn("g-date-picker", className)}
-					selected={startDate}
-					showMonthYearPicker={showMonthView}
-					onChange={handlerStartDate}
-					onSelect={onSelect}
-					renderDayContents={renderDay ? renderDay : renderDayContents}
-					renderMonthContent={renderMonth ? renderMonth : renderMonthContent}
-					inline
-				/>
-			</div>
-		</>
-	);
+          </div>
+        ))}
+      </>
+    );
+  };
+
+  const renderMonthContent = (month, shortMonth, longMonth, day) => {
+    const handlerSelectMonth = () => {
+      const fullYear = new Date(day).getFullYear();
+      setShowMonthView(false);
+      //   setCurrentMonth(new Date(`${fullYear}-01-${month + 1 < 10 ? `0${month + 1}` : month + 1}`));
+      handlerStartDate(new Date(`${fullYear}-01-${month + 1 < 10 ? `0${month + 1}` : month + 1}`));
+    };
+
+    return <span onClick={handlerSelectMonth}>{shortMonth}</span>;
+  };
+
+  return (
+    <>
+      <DatePickerNavigation
+        category={category}
+        onStartDate={handlerStartDate}
+        onShowMonthView={setShowMonthView}
+        currentDay={startDate}
+        onDateInput={handlerSetDateInput}
+        events={events}
+      />
+
+      <div className="wrapper-date-picker">
+        <DatePickerMain
+          {...props}
+          calendarClassName={cn("g-date-picker", className)}
+          selected={startDate}
+          showMonthYearPicker={showMonthView}
+          onChange={handlerStartDate}
+          onSelect={onSelect}
+          renderDayContents={renderDay ? renderDay : renderDayContents}
+          renderMonthContent={renderMonth ? renderMonth : renderMonthContent}
+          inline
+        />
+      </div>
+    </>
+  );
 };
 
 export default DatePickerView;
--- src/components/DatePicker/components/DatePickerNavigation/DatePickerNavigation.jsx
+++ src/components/DatePicker/components/DatePickerNavigation/DatePickerNavigation.jsx
@@ -1,104 +1,104 @@
-import React, {useEffect} from "react";
-import {useState} from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import DataCustom from "../DataCustom/DataCustom.jsx";
 import Button from "../../../Button/Button";
 
 import "./DatePickerNavigation.scss";
 
 const DatePickerNavigation = ({
-	                              category,
-	                              onStartDate,
-	                              onShowMonthView,
-	                              currentDay,
-	                              onDateInput,
-	                              events
-                              }) => {
-	const [filteredBySelectEvents, setFilteredBySelectEvents] = useState([]);
-	
-	const handlerTodayButton = () => {
-		onStartDate(new Date());
-	};
-	
-	const handlerMonthButton = () => {
-		onShowMonthView(true);
-	};
-	
-	// const handlerInputValue = (e) => {
-	// 	setInputValue(e.target.value);
-	// };
-	
-	const handlerSelectOptions = (e) => {
-		let currentOption = e.target.value;
+  category,
+  onStartDate,
+  onShowMonthView,
+  currentDay,
+  onDateInput,
+  events,
+}) => {
+  const [filteredBySelectEvents, setFilteredBySelectEvents] = useState([]);
 
-		if (currentOption === "all") {
-			setFilteredBySelectEvents(events);
-			return;
-		}
+  const handlerTodayButton = () => {
+    onStartDate(new Date());
+  };
 
-		// console.log(currentOption);
-		setFilteredBySelectEvents(events.filter((item) => item.category === currentOption));
+  const handlerMonthButton = () => {
+    onShowMonthView(true);
+  };
 
-		// console.log(filteredBySelectEvents);
-	};
-	
-	// useEffect(() => {
-	//   setFilteredBySelectEvents(events);
-	// }, [events]);
-	
-	return (
-		<div className="date-picker-navigation">
-			{/* <button>Month</button>
+  // const handlerInputValue = (e) => {
+  // 	setInputValue(e.target.value);
+  // };
+
+  const handlerSelectOptions = (e) => {
+    let currentOption = e.target.value;
+
+    if (currentOption === "all") {
+      setFilteredBySelectEvents(events);
+      return;
+    }
+
+    // console.log(currentOption);
+    setFilteredBySelectEvents(events.filter((item) => item.category === currentOption));
+
+    // console.log(filteredBySelectEvents);
+  };
+
+  // useEffect(() => {
+  //   setFilteredBySelectEvents(events);
+  // }, [events]);
+
+  return (
+    <div className="date-picker-navigation">
+      {/* <button>Month</button>
         <button>Week</button> */}
-			<Button
-				underlineView
-				classNames="date-picker-navigation-btn-today"
-				onClick={handlerTodayButton}
-			>
-				Today
-			</Button>
-			<Button
-				underlineView
-				classNames="date-picker-navigation-btn-today"
-				onClick={handlerMonthButton}
-			>
-				Month
-			</Button>
-			{/*<input*/}
-			{/*	className="date-picker-navigation-input-day"*/}
-			{/*	onChange={handlerInputValue}*/}
-			{/*	type="date"*/}
-			{/*	value={inputValue}*/}
-			{/*/>*/}
-			<DataCustom
-				selected={currentDay}
-				startDate={currentDay}
-				onChange={(date) => onDateInput(date)}
-			/>
-			<div className="filters">
-				<div>Filters</div>
-				<div className="date-picker-wrapper">
-					<select
-						className="date-picker-navigation-select"
-						name=""
-						id=""
-						onChange={handlerSelectOptions}
-					>
-						<option value="all">All</option>
-						{category.map((item, index) => (
-							<option className="date-picker-navigation-option" key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div>
-					{filteredBySelectEvents.map((item) => (
-						<div key={item.id}>{item.title}</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+      <Button
+        underlineView
+        classNames="date-picker-navigation-btn-today"
+        onClick={handlerTodayButton}
+      >
+        Today
+      </Button>
+      <Button
+        underlineView
+        classNames="date-picker-navigation-btn-today"
+        onClick={handlerMonthButton}
+      >
+        Month
+      </Button>
+      {/*<input*/}
+      {/*	className="date-picker-navigation-input-day"*/}
+      {/*	onChange={handlerInputValue}*/}
+      {/*	type="date"*/}
+      {/*	value={inputValue}*/}
+      {/*/>*/}
+      <DataCustom
+        selected={currentDay}
+        startDate={currentDay}
+        onChange={(date) => onDateInput(date)}
+      />
+      <div className="filters">
+        <div>Filters</div>
+        <div className="date-picker-wrapper">
+          <select
+            className="date-picker-navigation-select"
+            name=""
+            id=""
+            onChange={handlerSelectOptions}
+          >
+            <option value="all">All</option>
+            {category.map((item, index) => (
+              <option className="date-picker-navigation-option" key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          {filteredBySelectEvents.map((item) => (
+            <div key={item.id}>{item.title}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DatePickerNavigation;
