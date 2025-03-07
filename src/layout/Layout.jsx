96ddbd865cdf365bdd159ad71b4c31d4b7472153
Oleh Kyrylenko <oleh.kyrylenko@sunyempire.edu>
2025-03-07 14:31:50 -0500

    uese.exp

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -11,15 +11,14 @@ const Layout = ({ children, isLogin, isAddEvent, currentUser, onToken }) => {
   useEffect(() => {
     if (currentUser.exp) {
       setTimeoutTime(currentUser.exp * 1000 - new Date().getTime());
+      console.log(`Difference: ${timeoutTime / (1000 * 60)} minutes`);
+      const timeout = setTimeout(() => {
+        sessionStorage.setItem("token", "");
+        setLogout(true);
+      }, timeoutTime);
+
+      return () => clearTimeout(timeout);
     }
-    console.log(`Difference: ${timeoutTime / (1000 * 60)} minutes`);
-
-    const timeout = setTimeout(() => {
-      sessionStorage.setItem("token", "");
-      setLogout(true);
-    }, timeoutTime);
-
-    return () => clearTimeout(timeout);
   }, [timeoutTime, currentUser]);
 
   return (
